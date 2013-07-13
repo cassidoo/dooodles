5ce1de7e3ac59805141a8ba1d2d41dda96b3fe01
Cassidy Williams <cassidoo@outlook.com>
2013-07-13 09:02:08 -0700

    Created word generation and checking functions, and jQuery just in case

--- words.js
+++ words.js
@@ -1,9 +1,14 @@
-function generateWord()
-{
-	
+function getRandomWord(jsonWords) {
+    var words = JSON.parse(jsonWords);
+
+    var MAX_LENGTH = 10;
+    var wordLength = Math.floor(Math.random() * MAX_LENGTH) + 1;
+    var wordIndex = Math.floor(Math.random() * words[wordLength].length) + 1;
+
+    var word = words[wordLength][wordIndex];
 }
 
-function checkIfCorrect()
+function checkIfCorrect(guessedword, actualword)
 {
-	
+	return guessedword.toUpperCase() === actualword.toUpperCase();
 }
